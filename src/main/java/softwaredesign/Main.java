d595f2c218a257b02d740e5be7caffbab8140b85
Andres Lot Camarena <lotcamandres@gmail.com>
2023-02-13 17:00:18 +0100

    added a simple widnow launcher (testing purposes)

--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -1,7 +1,24 @@
 package softwaredesign;
 
-public class Main {
-    public static void main (String[] args){
-        System.out.println("Welcome to Software Design");
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+/**
+ * This is an example of a minimalistic FXGL game application.
+ *
+ * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
+ */
+public class Main extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings){
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
