2fa4cba269a9f67b0a98b1dec9ebf7898c0f9a98
Andres Lot Camarena <lotcamandres@gmail.com>
2023-03-27 17:46:24 +0200

    added background Image

--- src/main/java/softwaredesign/Background.java
+++ src/main/java/softwaredesign/Background.java
@@ -1,7 +1,11 @@
 package softwaredesign;
 
-import java.util.ArrayList;
+import javafx.scene.image.Image;
 
 public class Background extends Item {
-    //TODO: figure out images
+    Image backgroundImage;
+
+    public Background(Image backgroundImage) {
+        this.backgroundImage = backgroundImage;
+    }
 }
