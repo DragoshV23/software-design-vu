874ff523a291a326ef48e4c71b7a4688327f72ac
Andres Lot Camarena <lotcamandres@gmail.com>
2023-04-03 13:48:20 +0200

    added dead message and button func

--- src/main/java/softwaredesign/DeadUI.java
+++ src/main/java/softwaredesign/DeadUI.java
@@ -1,8 +1,14 @@
 package softwaredesign;
 
+import com.almasb.fxgl.dsl.FXGL;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class DeadUI extends BaseUI {
     private Pet pet = Pet.getInstance();
@@ -14,5 +20,29 @@ public class DeadUI extends BaseUI {
     }
     private void addAdditionalComponents() {
         Button resetButton = createIconButton("undo.png", getBottomBar());
+        String deadMessage = pet.getName() + checkCauseDead();
+        Label label = new Label(deadMessage);
+
+        label.setTextFill(Color.WHITE);
+        label.setFont(Font.loadFont(getClass().getResource("/assets/fonts/PressStart2P-Regular.ttf").toExternalForm(), 20));
+        getTopBar().getChildren().add(label);
+
+        resetButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Main.deleteSaveFile();
+            }
+        });
+    }
+     String checkCauseDead() {
+        if (pet.getHealth() <= 0) {
+            return "wasn't cleaned \nin a long time";
+        } else if (pet.getMood() <= 0) {
+            return "felt too lonely";
+        } else if (pet.getEnergy() <= 0) {
+            return "suffered from \nsleep deprivation";
+        } else {
+            return "starved to death";
+        }
     }
 }
