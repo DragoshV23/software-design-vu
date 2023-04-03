4d2817e6d8ea98b54a4117aac0422ae219a5d0cd
Andres Lot Camarena <lotcamandres@gmail.com>
2023-04-03 12:12:09 +0200

    added reset button dead (no action)

--- src/main/java/softwaredesign/DeadUI.java
+++ src/main/java/softwaredesign/DeadUI.java
@@ -1,5 +1,6 @@
 package softwaredesign;
 
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -7,7 +8,11 @@ public class DeadUI extends BaseUI {
     private Pet pet = Pet.getInstance();
     public DeadUI(HBox clockBar) {
         super(clockBar);
+        addAdditionalComponents();
         VBox ui = createUI(getTopUi(), getBottomBar());
         this.getChildren().add(ui);
+    }
+    private void addAdditionalComponents() {
+        Button resetButton = createIconButton("undo.png", getBottomBar());
     }
 }
